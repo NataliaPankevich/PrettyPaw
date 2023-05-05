65f8cb3d21601087c85c7bccab6aef72fde2b7a9
Natalia Pankevich <pankevicnatalia4@gmail.com>
2023-05-05 09:41:21 +0300

    2 lesson

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './index.html';
 //new modules
+import 'swiper/scss';
+import 'swiper/scss/pagination';
 import './index.scss';
+
+const videoBg = document.querySelector('.video-bg');
+videoBg.innerHTML = `
+    <source src="video/video.webm" type="video/webm"></source>
+    <source src="video/video.mp4" type="video/mp4"></source>
+`;
